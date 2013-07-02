766856ed48e7a26226aac27c9bd801b49b1063c4
Ryan Leckey <rleckey@concordusapps.com>
2013-07-02 06:49:15 -0700

    Doh..

--- src/alchemist/db/models.py
+++ src/alchemist/db/models.py
@@ -126,7 +126,7 @@ class Model(metaclass=ModelBase):
             db.session.commit()
 
 
-class Timestamp(db.Model):
+class Timestamp(Model):
     """Records when a model has been created and updated.
     """
 
