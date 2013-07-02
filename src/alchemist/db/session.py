b489e2040f77f66d8974f3c30f908130f0bcb05a
Ryan Leckey <rleckey@concordusapps.com>
2013-07-02 00:14:34 -0700

    Fix name error.

--- src/alchemist/db/session.py
+++ src/alchemist/db/session.py
@@ -8,7 +8,7 @@ class Session(sa.orm.Session):
     def __init__(self, *args, **kwargs):
         # TODO: Support multiple database routing.
         # Bind to the default database.
-        kwargs.setdefault(bind, settings['DATABASES']['default'])
+        kwargs.setdefault('bind', settings['DATABASES']['default'])
 
         # Continue the initialization.
         super().__init__(*args, **kwargs)
