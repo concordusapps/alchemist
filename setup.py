6a33315a985353b22502f48e6fb91004df11b03a
Ryan Leckey <rleckey@concordusapps.com>
2013-07-15 11:06:54 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alchemist',
-    version='0.2.0',
+    version='0.2.1',
     description='A server architecture built on top of a solid foundation '
                 'provided by flask, sqlalchemy, and various extensions.',
     classifiers=[
