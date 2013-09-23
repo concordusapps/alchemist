61ac22fdba34049e6ef6d24d16d5adf84f22096c
Ryan Leckey <rleckey@concordusapps.com>
2013-09-23 12:06:28 -0700

    Bump version.

--- src/alchemist/meta.py
+++ src/alchemist/meta.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-version = '0.2.17'
+version = '0.2.18'
 description = ('A server architecture built on top of a solid foundation '
                'provided by flask, sqlalchemy, and various extensions.')
