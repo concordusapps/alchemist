0c15ae20c7710a7faed7bbaf6a184207e1aaae89
Ryan Leckey <rleckey@concordusapps.com>
2013-11-02 14:06:43 -0700

    Set version.

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ setup(
         'git+git://github.com/concordusapps/wsgi-intercept.git'
         '#egg=wsgi_intercept-0.6.0',
 
-        'git+git://github.com/concordusapps/flask-script.git@edge'
-        '#egg=flask-script-edge',
+        'git+git://github.com/concordusapps/flask-script.git@0.6.2'
+        '#egg=flask-script-0.6.2',
 
         'https://launchpad.net/oursql/py3k/py3k-0.9.4/+download/'
         'oursql-0.9.4.tar.gz#egg=oursql-0.9.4',
@@ -53,7 +53,7 @@ setup(
         'termcolor >= 1.1, < 1.2',
         'flask >= 0.10',
         'blinker >= 1.3',
-        'flask-script == edge',
+        'flask-script == 0.6.2',
         'flask-components >= 0.1',
         'sqlalchemy >= 0.8',
         'sqlalchemy-utils >= 0.16',
