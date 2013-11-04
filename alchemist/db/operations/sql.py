5534e49a9de20bebc767cd9b294c050dea995829
Ryan Leckey <rleckey@concordusapps.com>
2013-11-04 14:16:15 -0800

    Skip if database doesn't exist.

--- alchemist/db/operations/sql.py
+++ alchemist/db/operations/sql.py
@@ -35,6 +35,9 @@ def op(expression, tables=None, test=None, primary=None, secondary=None,
 
         target = engine['default']
 
+        if not database_exists(target.url):
+            continue
+
         if not offline and test(target, table):
             continue
 
