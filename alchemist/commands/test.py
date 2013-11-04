e4671b877924bcc6d5385c2b19e2e76f61868b92
Ryan Leckey <rleckey@concordusapps.com>
2013-11-04 15:00:40 -0800

    Register fixture to place the test runner in an application context.

--- alchemist/commands/test.py
+++ alchemist/commands/test.py
@@ -24,11 +24,20 @@ class Test(Command):
         # This would execute tests at '<project>/tests'.
 
         name = kwargs.pop('name') or app.application.name
+        application = app._get_application_from_name(name)
 
         # Get the test directory.
 
         module = import_module(name)
         directory = path.join(path.dirname(module.__file__), 'tests')
+
+        # Register fixture to place the test runner in an application context.
+
+        @pytest.fixture(autouse=True, scope='session')
+        def fixture_application(request):
+            context = application.app_context()
+            context.push()
+            request.addfinalizer(lambda: context.pop())
 
         # Execute 'py.test' with the remaining arguments.
 
